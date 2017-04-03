50957aa4ee8e0f7531bd7335bc9b9ee9eae2ee6e
Anthony Luc <anthonyvanluc@gmail.com>
2017-04-03 00:36:48 -0400

    being working on pQueues in Python

---
+++ pQueue.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python2.7
+#
+###
+#   Title: pQueue.py
+#   Author: Anthony Luc
+#   Date: April 2, 2017
+#   Course: Data Structures 2017
+#   Assignment: Final Project
+#
+#   About: Practicing using the priority queue in python
+#
+#	Source: https://docs.python.org/2/library/queue.html
+#			http://stackoverflow.com/questions/9289614/how-to-put-items-into-priority-queues
+#
+###
+
+
+# Imported Libraries
+import os
+from Queue import PriorityQueue
+
+
+# Global Variables
+myPQ = Queue.PriorityQueue
+
+
+# Note: dir(myPQ)
+['__doc__',
+ '__init__',
+ '__module__',
+ '_get',
+ '_init',
+ '_put',
+ '_qsize',
+ 'empty',
+ 'full',
+ 'get',
+ 'get_nowait',
+ 'join',
+ 'put',
+ 'put_nowait',
+ 'qsize',
+ 'task_done']
+
