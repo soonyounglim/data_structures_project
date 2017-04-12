555f076717e3acf315bcba3fcca32196ca95543c
Donald <donaldvluc@gmail.com>
2017-04-12 17:33:41 -0400

    Finished retreiving next page

--- reddit.py
+++ reddit.py
@@ -12,9 +12,9 @@ import re
 
 
 # Define Variables:
-headers  = {'user-agent': 'reddit-{}'.format(os.environ['USER'])}
-user = 'madviet'
-page_count = '25'
+HEADERS  = {'user-agent': 'reddit-{}'.format(os.environ['USER'])}
+USER = 'spez'
+POSTS_PER_PAGE = 25
 
 # Define Functions:
 def usage(status):
@@ -24,53 +24,33 @@ def usage(status):
 	)
 	sys.exit(status)
 
+def get_initial_page(TYPE):
+	web_address = 'https://www.reddit.com/u/{}/submitted/.json'.format(USER)
+	url = requests.get(web_address,headers=HEADERS).json()
+	print web_address
+	return url["data"]["after"] # Saves the pointer to the next page.
+
+def get_next_page(TYPE, COUNT, NEXT_PAGE):
+	web_address = 'https://www.reddit.com/u/{}/{}/.json?count={}&after={}'.format(USER, TYPE, COUNT, NEXT_PAGE)
+	url = requests.get(web_address,headers=HEADERS).json()
+	print web_address
+	return url['data']['after']
+
 
 # Main Execution:
 args = sys.argv[1:]
 while len(args) and args[0].startswith('-') and len(args[0]) > 1:
 	arg = args.pop(0)
-
 	if arg == '-h':
 		usage(0)
 	elif arg == '-u':
-		user = args.pop(0)
+		USER = args.pop(0)
 	else:
 		usage(1)
 
 # Get Posts:
-web_address = 'https://www.reddit.com/u/'+user+'/comments/.json'
-url = requests.get(web_address,headers=headers).json()
-comment = url["data"]["children"][0]["data"]["body"] # Saves the pointer to the next page.
-print comment
-
-'''while (next_page):
-	print next_page
-	web_address = 'https://www.reddit.com/u/'+user+'/submitted/?count='+page_count+'&after='+next_page
-	url = requests.get(web_address,headers=headers).json()
-	next_page = url["data"]["after"]'''
-
-'''i = 0
-tryval = 1;
-while tryval:
-	try:
-		title = url["data"]["children"][i]["data"]["link_title"]
-		link_author = url["data"]["children"][i]["data"]["link_author"]
-		author = url["data"]["children"][i]["data"]["author"]
-		#body = url["data"]["children"][i]["data"]["body"]
-		#subreddit = url["data"]["children"][i]["data"]["subreddit"]
-		if author == link_author:
-			print "This is a post"
-			print title	
-		
-		print "Title: "+title
-		print "Link Author: "+link_author
-		print "Author: "+author
-		print "Body: "+body
-		print "Subreddit: "+subreddit
-		
-	except KeyError:
-		#print "error"
-		tryval = 0
-		#sys.exit(1)
-	i = i + 1'''
-
+next_page = get_initial_page('submitted')
+count = POSTS_PER_PAGE
+while (next_page):
+	next_page = get_next_page('submitted', count, next_page)
+	count = count + POSTS_PER_PAGE
