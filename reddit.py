9552e0207d42dc9e14427920385b8f3ed7e95b63
Anthony Luc <anthonyvanluc@gmail.com>
2017-04-23 23:33:08 -0400

    update

--- reddit.py
+++ reddit.py
@@ -9,7 +9,9 @@ import os
 import sys
 import requests
 import re
-
+# Heap
+import collections
+from heapq import heappush, heappop, nlargest, nsmallest
 
 # Define Variables:
 USER = 'madviet'
@@ -17,15 +19,23 @@ POSTS_PER_PAGE = 25
 HEADERS  = {'user-agent': 'reddit-{}'.format(os.environ['USER'])}
 TYPE = ''
 
+# Heap Variables
+NUMTOPBOT 	    = 3
+COMMENTHEAP 	= []
+POSTHEAP 		= []
+
 # Define Dictionaries:
 interests = {}
 family = {}
 
+# Define Object for use in the heap
+myPair = collections.namedtuple('myPair', ['score', 'body'])
 
 # Define Functions:
 def usage(status):
-	print '''Usage: {} -u USER
-	-u USER		The reddit user you wish to look up'''.format(
+	print '''Usage: {} ...
+	-u USER	          The reddit user you wish to look up
+	-n NUMTOP/BOT    The number of top and bottom comment/post scores'''.format(
 		os.path.basename(sys.argv[0])
 	)
 	sys.exit(status)
@@ -41,9 +51,10 @@ def parse_json(URL_JSON):
 	for i in range(0, len(URL_JSON["data"]["children"])):
 		# Get metadata for comments.
 		if (TYPE == 'comments'):
-			comment_title = get_data(URL_JSON, i, 'link_title')
-			comment_author = get_data(URL_JSON, i, 'link_author')
 			body = get_data(URL_JSON, i, 'body')
+			
+			score = get_data(URL_JSON, i, 'score')
+			store_heap(score, body, 'comment')
 			
 			find_family(body)									# Grep For Comments Based On Family.
 
@@ -51,6 +62,9 @@ def parse_json(URL_JSON):
 		else:
 			post_title = get_data(URL_JSON, i, 'title')
 			self_post = get_data(URL_JSON, i, 'is_self')
+
+			score = get_data(URL_JSON, i, 'score')
+			store_heap(score, post_title, 'post')
 
 			find_family(post_title)
 		
@@ -69,9 +83,9 @@ def find_family(BODY):
 		word = word.strip();
 		if (re.search(word, BODY)):
 			if word in family:
-                            family[word] = family[word] + 1
-                        else:
-                            family[word] = 1
+				family[word] = family[word] + 1
+			else:
+				family[word] = 1
 
 
 def get_data(URL_JSON, INDEX, DATA):
@@ -99,26 +113,51 @@ def print_family():
 	for key, value in family.items():
 	        print '{}\t{}'.format(key, value)
 
+# Heap functions
+def store_heap(SCORE, BODY, TYPE):
+	node = myPair(score = SCORE, body = BODY)
+	if TYPE == 'comment':
+		heappush(COMMENTHEAP, node)
+	else:
+		heappush(POSTHEAP, node)
+
+def print_top_bot(NUMTOPBOT):
+	print "Comments:"
+	a = nlargest(NUMTOPBOT, enumerate(COMMENTHEAP), key=lambda x: x[1])
+	print_heap(a)
+	a = nsmallest(NUMTOPBOT, enumerate(COMMENTHEAP), key=lambda x: x[1])
+	print_heap(a)
+	print "Posts:"
+	a = nlargest(NUMTOPBOT, enumerate(POSTHEAP), key=lambda x: x[1])
+	print_heap(a)
+	a = nsmallest(NUMTOPBOT, enumerate(POSTHEAP), key=lambda x: x[1])
+	print_heap(a)
+
+def print_heap(heap):   # Function that prints the heap
+	for k,v in enumerate(heap):
+		print "#", k+1, "\tScore:", v[1][0], "\tBody:", v[1][1], "\n"
 
 # Main Execution:
-args = sys.argv[1:]
-while len(args) and args[0].startswith('-') and len(args[0]) > 1:
-	arg = args.pop(0)
-	if arg == '-h':
-		usage(0)
-	elif arg == '-u':
-		USER = args.pop(0)
-	else:
-		usage(1)
+if __name__ == '__main__':
+	args = sys.argv[1:]
+	while len(args) and args[0].startswith('-') and len(args[0]) > 1:
+		arg = args.pop(0)
+		if arg == '-h':
+			usage(0)
+		elif arg == '-n':
+			NUMTOPBOT = int(args.pop(0))
+		elif arg == '-u':
+			USER = args.pop(0)
+		else:
+			usage(1)
 
+	# Get Comments / Posts:
+	TYPE = 'comments'
+	parse_user_history(TYPE)
+	TYPE = 'submitted'
+	parse_user_history(TYPE)
 
-# Get Comments / Posts:
-TYPE = 'comments'
-parse_user_history(TYPE)
-TYPE = 'submitted'
-parse_user_history(TYPE)
-
-
-# Print Overview:
-print_interests()
-print_family()
+	# Print Overview:
+	print_top_bot(NUMTOPBOT)
+	print_interests()
+	print_family()
